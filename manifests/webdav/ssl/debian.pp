2f7fb9ce78c89c8dcfe7d33b3dd8f508246257ce
andreias <andrei.asan@cegeka.com>
2018-01-17 14:53:27 +0100

    fix wildfly dep + fix other cases

--- manifests/webdav/ssl/debian.pp
+++ manifests/webdav/ssl/debian.pp
@@ -2,7 +2,7 @@ class cegeka_apache::webdav::ssl::debian {
   include cegeka_apache::webdav::base
 
   case $::lsbdistcodename {
-    etch: {
+    'etch': {
       # cf: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=420101
       file {'/var/lock/apache2':
         ensure  => directory,
